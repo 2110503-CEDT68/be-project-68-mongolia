9aff02b5e013341bd2440e49a4bc2f1c17131c24
nantapong srihattaphadungkit <13nantapong.srohattaphadungkit@gmail.com>
2026-02-25 11:45:02 +0700

    add models/MessageShop.js

---
+++ models/MassageShop.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const MassageShopSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true,'Please add a name'],
+        unique: true,
+        trim: true,
+        maxlength: [50, 'Name can not be more than 50 characters']
+    },
+    address: {
+        type: String,
+        required: [true, 'Please add an address']
+    },
+    tel: {
+        type: String
+    },
+    openTime: {
+        type: String,
+        required: [true, 'Please add an openTime']
+    },
+    closeTime: {
+        type: String,
+        required: [true, 'Please add a closeTime']
+    }
+},{
+    toJSON: {virtuals:true},
+    toObject:{virtuals:true}
+});
+
+//Reverse populate with virtuals
+MassageShopSchema.virtual('reservations',{
+    ref: 'MassageReservation',
+    localField: '_id',
+    foreignField: 'shop',
+    justOne: false
+});
+module.exports=mongoose.model('MassageShop', MassageShopSchema);
